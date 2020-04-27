bf3cf21ee0c26b0935d63d2d893f2064ec5cc108
krisfromtama <62895586+krisfromtama@users.noreply.github.com>
2020-04-27 16:41:27 +0200

    Add files via upload

---
+++ Script-Java.js
@@ -0,0 +1,31 @@
+$(".etape-content").eq(0).hide();
+$(".auto").click(function () {
+	$(".etape-content").eq(0).slideDown("slow");
+})
+$(".auto").click(function () {
+	$(".etape-content").eq(0).hide();
+})
+
+$(".etape-content").eq(1).hide();
+$(".idee").click(function () {
+	$(".etape-content").eq(1).slideDown("slow");
+})
+$(".idee").click(function () {
+	$(".etape-content").eq(1).hide();
+})
+
+$(".etape-content").eq(2).hide();
+$(".wagon").click(function () {
+	$(".etape-content").eq(2).slideDown("slow");
+})
+$(".wagon").click(function () {
+	$(".etape-content").eq(2).hide();
+})
+
+$(".etape-content").eq(3).hide();
+$(".code").click(function () {
+	$(".etape-content").eq(3).slideDown("slow");
+})
+$(".code").click(function () {
+	$(".etape-content").eq(3).hide();
+})
